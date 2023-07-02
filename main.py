63377c258dd69de62c08b0017966933f9e1162eb
Artem <111689234+ArtRiaz@users.noreply.github.com>
2023-07-02 16:10:56 +0200

    +photo + text

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from aiogram import types, executor
 from create_bot import dp, bot
-from aiogram.dispatcher.filters import Command
+from aiogram.dispatcher.filters import Text
+
 
 async def on_startup(_):
     print('Бот запущен')
 
-@dp.message_handler(Command='start')
+
+@dp.message_handler(Text(equals='start'))
 async def start(message: types.Message):
     await message.answer('Привет')
 
-@dp.message_handler(Command='photo')
+
+@dp.message_handler(Text(equals='photo'))
 async def get_photo(message: types.Message):
     with open('/Users/artem/Desktop/test.jpg', 'rb') as photo:
         await bot.send_photo(chat_id=message.from_user.id, photo=photo)
